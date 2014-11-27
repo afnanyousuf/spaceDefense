e765444ef6bc8e3406750571ec202a79d855ad85
afnanyousuf <afnan3@gmail.com>
2014-11-26 22:33:50 -0500

    dont need no main
    game states workin

--- spaceDefense.py
+++ spaceDefense.py
@@ -6,35 +6,35 @@ from pygame.locals import *
 screens=["main","game","paused","result","tutorial"]
 gameState = screens[0]
 
-def main():
-    pygame.init()
-    size = (600,800)
-    screen = pygame.display.set_mode(size)
-    done = False
-    clock = pygame.time.Clock()
+pygame.init()
+size = (800,600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Space Defender")
+done = False
+clock = pygame.time.Clock()
 
-    while True:
-        if(gameState=="main"):
-            screen.fill(pygame.Color(255,0,0))
+while True:
+    if(gameState=="main"):
+        screen.fill(pygame.Color(255,0,0))
 
-            for event in pygame.event.get():
-                if event.type==QUIT:
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == MOUSEBUTTONUP:
-                    gameState = "game"
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONUP:
+                gameState = "game"
 
-        if(gameState=="game"):
-            screen.fill(pygame.color(0,0,0)
-
-            for event in pygame.event.get():
-                if event.type==QUIT:
-                    pygame.quit()
-                    sys.exit()
+    if(gameState=="game"):
+        screen.fill(pygame.Color(0,0,0))
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                sys.exit()
                 
                         
             
-
+    pygame.display.update()
+    clock.tick(60)
 
         
         
